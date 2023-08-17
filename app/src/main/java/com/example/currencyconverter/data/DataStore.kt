c51e63a917e43a47e97e28e34fe4fdd46b305814
Nicole <nicole.georgievva@gmail.com>
2023-08-17 22:02:12 +0300

    Setup project - DataStore and requests

---
+++ app/src/main/java/com/example/currencyconverter/data/DataStore.kt
@@ -0,0 +1,11 @@
+package com.example.currencyconverter.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "myapp-data")
+
+val NAME_KEY = stringPreferencesKey("name")
---
+++ app/src/main/java/com/example/currencyconverter/request/KtorClient.kt
@@ -0,0 +1,17 @@
+package com.example.currencyconverter.request
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.serialization.kotlinx.json.json
+
+val client = HttpClient {
+    install(ContentNegotiation) {
+        json()
+    }
+}
+
+suspend fun fetchTodos(): HttpResponse {
+    return client.get("http://0.0.0.0:8080/todo")
+}
