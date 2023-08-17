c0ffffb6873c3ad136619fdc8d683e39b7206c98
Nicole <nicole.georgievva@gmail.com>
2023-08-17 22:23:07 +0300

    Finish the basic HomeScreen's UI

--- app/src/main/java/com/example/currencyconverter/screen/HomeScreen.kt
+++ app/src/main/java/com/example/currencyconverter/screen/HomeScreen.kt
@@ -1,10 +1,12 @@
 package com.example.currencyconverter.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -14,9 +16,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,16 +31,41 @@ fun HomeScreen() {
             .fillMaxSize()
             .padding(12.dp)
     ) {
-        Text(text = "Hello, User!")
+        Text(text = "Hello, User!", fontWeight = FontWeight.Bold, fontSize = 32.sp)
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(24.dp))
 
         var convertFrom by rememberSaveable { mutableStateOf("") }
+        var convertTo by rememberSaveable { mutableStateOf("") }
 
-        TextField(
-            value = convertFrom,
-            onValueChange = { convertFrom = it },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
+        Row(verticalAlignment = Alignment.Bottom) {
+            TextField(
+                value = convertFrom,
+                onValueChange = { convertFrom = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(text = "$", fontWeight = FontWeight.Bold)
+        }
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        Text(text = "=", fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        Row(verticalAlignment = Alignment.Bottom) {
+            TextField(
+                value = convertTo,
+                onValueChange = { convertTo = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(text = "BGN", fontWeight = FontWeight.Bold)
+        }
     }
 }
