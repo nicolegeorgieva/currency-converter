61faac926363a9febbd6808b0a149ca906b1ac98
Nicole <nicole.georgievva@gmail.com>
2023-08-22 21:41:23 +0300

    WIP: CountStudyTime's UI

--- app/src/main/java/com/example/currencyconverter/screen/CountStudyTime.kt
+++ app/src/main/java/com/example/currencyconverter/screen/CountStudyTime.kt
@@ -1,2 +1,81 @@
 package com.example.currencyconverter.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CountStudyTime() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
+        val hoursInput = rememberSaveable { mutableStateOf("") }
+        val minsInput = rememberSaveable { mutableStateOf("") }
+
+        TimeInputRow(
+            hours = hoursInput.value,
+            onHoursChange = { hoursInput.value = it },
+            mins = minsInput.value,
+            onMinsChange = { minsInput.value = it }
+        )
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimeInputRow(
+    hours: String, onHoursChange: (String) -> Unit,
+    mins: String, onMinsChange: (String) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TextField(
+            modifier = Modifier.width(124.dp),
+            value = hours,
+            onValueChange = {
+                onHoursChange(it)
+            },
+            label = { Text("Hours") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(text = ":", fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        TextField(
+            modifier = Modifier.width(124.dp),
+            value = mins,
+            onValueChange = {
+                onMinsChange(it)
+            },
+            label = { Text("Mins") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true
+        )
+    }
+}
