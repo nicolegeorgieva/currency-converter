1d9ca20ad94e9e78261dfb3927b60f350523c888
Nicole <nicole.georgievva@gmail.com>
2023-09-07 16:38:13 +0300

    Add tests for CurrencyConverter

--- app/src/test/java/com/example/currencyconverter/home/CurrencyConverterTest.kt
+++ app/src/test/java/com/example/currencyconverter/home/CurrencyConverterTest.kt
@@ -20,4 +20,34 @@ class CurrencyConverterTest : FreeSpec({
             100.0
         ) shouldBe 182.24299065420558
     }
+
+    "null arguments" - {
+        "Should return null for 2 null arguments" {
+            currencyConverter.exchangeUsdToBgn(
+                null,
+                null
+            ) shouldBe null
+        }
+
+        "Should return null for null exchangeRatesResponse" {
+            currencyConverter.exchangeUsdToBgn(
+                null,
+                50.0
+            ) shouldBe null
+        }
+
+        "Should return null for null hourlyRateInUsd" {
+            currencyConverter.exchangeUsdToBgn(
+                ExchangeRatesDataSource.ExchangeRatesResponse(
+                    date = "06.09.2023",
+                    eur = ExchangeRatesDataSource.Rates(
+                        1.95,
+                        1.07
+                    )
+                ),
+                null
+            ) shouldBe null
+        }
+    }
+
 })
