7dc88cba78efe32f19b3e041da6b5d1069599793
Nicole <nicole.georgievva@gmail.com>
2023-09-07 16:02:49 +0300

    Add tests for CurrencyConverter

---
+++ app/src/test/java/com/example/currencyconverter/home/CurrencyConverterTest.kt
@@ -0,0 +1,23 @@
+package com.example.currencyconverter.home
+
+import com.example.currencyconverter.screen.home.CurrencyConverter
+import com.example.currencyconverter.screen.home.ExchangeRatesDataSource
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class CurrencyConverterTest : FreeSpec({
+    val currencyConverter = CurrencyConverter()
+
+    "100$ should be ~182.0 BGN" {
+        currencyConverter.exchangeUsdToBgn(
+            ExchangeRatesDataSource.ExchangeRatesResponse(
+                date = "06.09.2023",
+                eur = ExchangeRatesDataSource.Rates(
+                    1.95,
+                    1.07
+                )
+            ),
+            100.0
+        ) shouldBe 182.24299065420558
+    }
+})
--- app/src/test/java/com/example/currencyconverter/home/MonthlySalaryCalculatorTest.kt
+++ app/src/test/java/com/example/currencyconverter/home/MonthlySalaryCalculatorTest.kt
@@ -7,12 +7,12 @@ import io.kotest.matchers.shouldBe
 class MonthlySalaryCalculatorTest : FreeSpec({
     val monthlySalaryCalculator = MonthlySalaryCalculator()
 
-    "monthly hours should be 173.0 for 40h per week, 52 weeks per year and 12mo per year" {
+    "monthly hours should be ~173.0 for 40h per week, 52 weeks per year and 12mo per year" {
         monthlySalaryCalculator.monthlyHours shouldBe 173.33333333333334
     }
 
     "calculate monthly salary" - {
-        "should return 8666.666666666668 for 50.0 hourly rate and 173.33333333333334 monthly hours" {
+        "should return ~8666.0 for 50.0 hourly rate and 173.33333333333334 monthly hours" {
             monthlySalaryCalculator.calculateMonthlySalary(
                 50.0,
                 173.33333333333334
