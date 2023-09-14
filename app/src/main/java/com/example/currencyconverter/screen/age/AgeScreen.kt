f75e53a596f562bafff5e8120d605773feb18c76
Nicole <nicole.georgievva@gmail.com>
2023-09-14 13:09:02 +0300

    Refactor AgeScreen

--- app/src/main/java/com/example/currencyconverter/screen/age/AgeScreen.kt
+++ app/src/main/java/com/example/currencyconverter/screen/age/AgeScreen.kt
@@ -2,29 +2,47 @@ package com.example.currencyconverter.screen.age
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.currencyconverter.Screen
 import com.example.currencyconverter.component.BackButton
 import com.example.currencyconverter.screenState
+import com.example.currencyconverter.ui.theme.CurrencyConverterTheme
+
+@Composable
+fun AgeScreen() {
+    val viewModel: AgeViewModel = viewModel()
+    val uiState = viewModel.uiState()
+
+    UI(
+        state = uiState,
+        onEvent = {
+            viewModel.onEvent(it)
+        }
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AgeScreen() {
+private fun UI(
+    state: AgeUiState,
+    onEvent: (AgeEvent) -> Unit
+) {
     Column {
-        val viewModel: AgeViewModel = viewModel()
-        val uiState = viewModel.uiState()
-
         LaunchedEffect(Unit) {
-            viewModel.onEvent(AgeEvent.OnStart)
+            onEvent(AgeEvent.OnStart)
         }
 
         BackButton {
@@ -34,11 +52,27 @@ fun AgeScreen() {
         Spacer(modifier = Modifier.height(12.dp))
 
         TextField(
-            value = uiState.age,
+            value = state.age,
             onValueChange = {
-                viewModel.onEvent(AgeEvent.OnChangeAge(it))
+                onEvent(AgeEvent.OnChangeAge(it))
             },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
+    }
+}
+
+@Preview
+@Composable
+fun AgeUiPreview() {
+    CurrencyConverterTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            UI(
+                state = AgeUiState("24"),
+                onEvent = {}
+            )
+        }
     }
 }
