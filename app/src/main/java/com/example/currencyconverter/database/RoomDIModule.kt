22b9b21150168dd549326e03c996f024e98bc94b
Nicole <nicole.georgievva@gmail.com>
2023-09-14 19:24:19 +0300

    Setup Database

---
+++ app/src/main/java/com/example/currencyconverter/database/RoomDIModule.kt
@@ -0,0 +1,27 @@
+package com.example.currencyconverter.database
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RoomDIModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext
+        context: Context
+    ): MyDatabase {
+        return Room.databaseBuilder(
+            context,
+            MyDatabase::class.java, "my-database"
+        ).build()
+    }
+}
