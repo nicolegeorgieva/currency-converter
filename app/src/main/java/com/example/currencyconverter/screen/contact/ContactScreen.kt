c9ac619cbefa5322c5d4f74d0af2cc8efb2ecaef
Nicole <nicole.georgievva@gmail.com>
2023-09-18 15:14:48 +0300

    Refactor ContactScreen

--- app/src/main/java/com/example/currencyconverter/screen/contact/ContactScreen.kt
+++ app/src/main/java/com/example/currencyconverter/screen/contact/ContactScreen.kt
@@ -42,9 +42,7 @@ fun ContactScreen() {
 
     ContactUi(
         uiState = uiState,
-        onEvent = {
-            viewModel.onEvent(it)
-        }
+        onEvent = viewModel::onEvent
     )
 }
 
