70dd6945665b5cddb34ba3caf46fddbc07ce7e2e
Nicole <nicole.georgievva@gmail.com>
2023-09-21 14:02:54 +0300

    Refactor ExchangeRatesDataSource

--- app/src/main/java/com/example/currencyconverter/screen/home/ExchangeRatesDataSource.kt
+++ app/src/main/java/com/example/currencyconverter/screen/home/ExchangeRatesDataSource.kt
@@ -3,12 +3,17 @@ package com.example.currencyconverter.screen.home
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
 import javax.inject.Inject
 
 class ExchangeRatesDataSource @Inject constructor(
     private val client: HttpClient
 ) {
+    private val apiUrl =
+        "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json"
+
     @Serializable
     data class ExchangeRatesResponse(
         val date: String,
@@ -22,9 +27,15 @@ class ExchangeRatesDataSource @Inject constructor(
     )
 
     suspend fun fetchExchangeRates(): ExchangeRatesResponse {
-        val response =
-            client.get("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json")
+        return try {
+            withContext(Dispatchers.IO) {
+                val response = client.get(apiUrl)
 
-        return response.body<ExchangeRatesResponse>()
+                response.body<ExchangeRatesResponse>()
+            }
+        } catch (e: Exception) {
+            // TODO - Handle that case
+            ExchangeRatesResponse(date = "", eur = Rates(bgn = 1.0, usd = 1.0))
+        }
     }
 }
